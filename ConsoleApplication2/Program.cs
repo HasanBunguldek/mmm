6f307758e64a6f51cd98c3d5fdbedd921a46af32
HasanBun <hasan_bunguldek@hotmail.com>
2019-03-03 17:15:27 +0300

    nn

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba Dünya");
+            Console.WriteLine("deneme");
         }
     }
 }
